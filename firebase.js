f9454167504830039646fe434f8acfb6994d043f
kevokevo426-cmd <kevokevo426@gmail.com>
2026-08-16 09:59:31 -0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyAXwKw7n28LiJx2W4PBkQG7_glL0sVog8M",
+    authDomain: "patanisha-49561.firebaseapp.com",
+    projectId: "patanisha-49561",
+    storageBucket: "patanisha-49561.firebasestorage.app",
+    messagingSenderId: "363879415134",
+    appId: "1:363879415134:web:81b99ab93290a3e45723cf"
+  };
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+</script>
